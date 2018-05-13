5e8b42f483f13dc296c3a85c61b81498c0bf5804
noujoudb <b.noujoud95@gmail.com>
2018-05-13 16:30:04 +0100

    'ok'

--- index.js
+++ index.js
@@ -1,28 +1,20 @@
-
-
+const vtoken = process.env.FB_VERIFY_ACCESS_TOKEN
 'use strict'
-
-//1
-const cool = require('cool-ascii-faces')
 const express = require('express')
-const path = require('path')
-const PORT = process.env.PORT || 5000
-
-express()
-  .use(express.static(path.join(__dirname, 'public')))
-  .set('views', path.join(__dirname, 'views'))
-  .set('view engine', 'ejs')
-  .get('/', (req, res) => res.render('pages/index'))
-  .get('/cool', (req, res) => res.send(cool()))
-  .listen(PORT, () => console.log(`Listening on ${ PORT }`))
-
-
-  /********************/
-  const bodyParser = require('body-parser')
+const bodyParser = require('body-parser')
 const request = require('request')
 const app = express()
+const assert = require('assert')
+
+var facebook = require('./facebook.js')
+
 
 app.set('port', (process.env.PORT || 5000))
+
+// Spin up the server
+app.listen(app.get('port'), function() {
+    console.log('running on port', app.get('port'))
+})
 
 // Process application/x-www-form-urlencoded
 app.use(bodyParser.urlencoded({extended: false}))
@@ -32,162 +24,24 @@ app.use(bodyParser.json())
 
 // Index route
 app.get('/', function (req, res) {
-	res.send('Hello world, I am a chat bot')
+    res.send('Hello world, I am a chat bot')
 })
-/*
-app.get('/webhook/', function (req, res) {
-	if (req.query['hub.verify_token'] === 'my_voice_is_my_password_verify_me') {
-		res.send(req.query['hub.challenge'])
-	}
-	res.send('Error, wrong token')
-})
-*/
+
+// for Facebook verification
+app.get('/webhook/', facebook.VerificationToken)
+
 // Posting to the webhook and Facebook messenger application.
-app.post('/webhook/', function (req, res) {
-    let messaging_events = req.body.entry[0].messaging
-    for (let i = 0; i < messaging_events.length; i++) {
-	    let event = req.body.entry[0].messaging[i]
-	    let sender = event.sender.id
-	    if (event.message && event.message.text) {
-		    let text = event.message.text
-		    if (text === 'Generic') {
-			    sendGenericMessage(sender)
-		    	continue
-		    }
-		    sendTextMessage(sender, "Text received, echo: " + text.substring(0, 200))
-	    }
-    }
-    res.sendStatus(200)
-})
+app.post('/webhook/', facebook.postMessages)
 
-// Spin up the server
-app.listen(app.get('port'), function() {
-	console.log('running on port', app.get('port'))
-})
-
-/****************/
-  
- app . post ( ' / webhook / ' , fonction ( req , res ) {
-     laisser messaging_events =  req . body . entry [ 0 ]. message 
-    pour ( let i =  0 ; i <  messaging_events . longueur ; i ++ ) {
-	     let  event  =  req . corps . entrée [ 0 ].messagerie [i]
-	     laisser l' expéditeur =  événement . expéditeur . id 
-	    if ( événement . message  &&  événement . message . texte ) {
-		     let text =  événement . un message . texte 
-		    sendTextMessage (expéditeur, " Texte reçu, écho: "  +  texte . substring ( 0 , 200 )) 
-	    } 
-    } res . sendStatus
-    ( 200 ) 
-}) 
-
-const  token  =  processus . env . FB_PAGE_ACCESS_TOKEN
-function sendTextMessage(sender, text) {
-    let messageData = { text:text }
-    request({
-        url: 'https://graph.facebook.com/v2.6/me/messages',
-        qs: {access_token:token},
-        method: 'POST',
-        json: {
-            recipient: {id:sender},
-            message: messageData,
-        }
-    }, function(error, response, body) {
-        if (error) {
-            console.log('Error sending messages: ', error)
-        } else if (response.body.error) {
-            console.log('Error: ', response.body.error)
-        }
-    })
-}
+facebook.facebookDemarre();
+facebook.facebookMenu();
 
 
-function sendMenuMessage(sender) {
-    let messageData = {
-        "attachment": {
-            "type": "template",
-            "payload": {
-                "template_type": "generic",
-                "elements": [{
-						"title": "SURE ",
-						"image_url":"https://botlist.co/system/BotList/Bot/logos/000/000/613/medium/Profile.png",
-						"buttons": [{
-							"type": "web_url",
-							"url": "http://surebot.io/",
-							"title": "Acces"
-						}]/*,
-					},{
-                    "title": "Adecco France",
-                    "image_url":"https://pbs.twimg.com/media/ClfMG1HWEAAl_EM.jpg",
-                    "buttons": [{
-                        "type": "web_url",
-                        "url": "https://www.messenger.com/t/adecco.france",
-                        "title": "Acces"
-                    }],
-                }, {
-                    "title": "Drift BOT",
-                    "image_url": "https://i.ytimg.com/vi/AHsspE09SvU/maxresdefault.jpg",
-                    "buttons": [{
-                        "type": "web_url",
-                        "url": "https://www.drift.com",
-                        "title": "Acces"
-                    }],*/
-                }]
-            }
-        }
-    }
-    request({
-        url: 'https://graph.facebook.com/v2.6/me/messages',
-        qs: {access_token:token},
-        method: 'POST',
-        json: {
-            recipient: {id:sender},
-            message: messageData,
-        }
-    }, function(error, response, body) {
-        if (error) {
-            console.log('Error sending messages: ', error)
-        } else if (response.body.error) {
-            console.log('Error: ', response.body.error)
-        }
-    })
-}
 
- app.post('/webhook/', function (req, res) {
-    let messaging_events = req.body.entry[0].messaging
-    for (let i = 0; i < messaging_events.length; i++) {
-      let event = req.body.entry[0].messaging[i]
-      let sender = event.sender.id
-      if (event.message && event.message.text) {
-  	    let text = event.message.text
-  	    if (text === 'Menu') {
-  		    sendMenuMessage(sender)
-  		    continue
-  	    }
-  	    sendTextMessage(sender, "Text received, echo: " + text.substring(0, 200))
-      }
-      if (event.postback) {
-  	    let text = JSON.stringify(event.postback)
-  	    sendTextMessage(sender, "Postback received: "+text.substring(0, 200), token)
-  	    continue
-      }
-    }
-    res.sendStatus(200)
-  })
-/************************************/ 
-  
-  //2
-app.get('/times', (req, res) => {
-  let result = ''
-  const times = process.env.TIMES || 5
-  for (i = 0; i < times; i++) {
-    result += i + ' '
-  }
-  res.send(result)
-})
 
   /****************************************************************************************************************************************/
 
-console.log("Test"); 
+//.log("Test"); 
 /*  
   const bodyParser = require('body-parser')
 const request = require('request')
